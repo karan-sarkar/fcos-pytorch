83eb2c55febc21f8e428e08783c3ef8ccb232432
karan-sarkar <karan@sarkarfamily.org>
2021-04-14 02:32:13 -0400

    Print Classes.

--- loss.py
+++ loss.py
@@ -81,7 +81,10 @@ class SigmoidFocalLoss(nn.Module):
         term2 = p ** gamma * torch.log(1 - p)
 
         # print(term1.sum(), term2.sum())
-
+        print(t)
+        print(class_ids)
+        print(t.shape)
+        print(class_ids.shape)
         loss = (
             -(t == class_ids).float() * alpha * term1
             - ((t != class_ids) * (t >= 0)).float() * (1 - alpha) * term2
