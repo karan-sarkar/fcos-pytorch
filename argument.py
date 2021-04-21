6072aa48dd029ec27b462fa2ee8f652c6ad88d9b
karan-sarkar <karan@sarkarfamily.org>
2021-04-21 04:06:42 -0400

    Fixed arguments

--- argument.py
+++ argument.py
@@ -10,7 +10,7 @@ def get_argparser():
     parser.add_argument('--batch', type=int, default=16)
     parser.add_argument('--epoch', type=int, default=24)
     parser.add_argument('--n_save_sample', type=int, default=5)
-    parser.add_argument('--ckpt', type=str)
+    parser.add_argument('--ckpt', type=int)
     parser.add_argument('path', type=str)
     parser.add_argument('path2', type=str)
 
