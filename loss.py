0b0721b3437cc78a692d44113ffb0e1764564e62
karan-sarkar <karan@sarkarfamily.org>
2021-04-23 10:45:38 -0400

    Use Focal Loss.

--- loss.py
+++ loss.py
@@ -5,24 +5,39 @@ import torch.nn.functional as F
 
 INF = 100000000
 
+from torch.autograd import Variable
+
 class FocalLoss(nn.Module):
-    
-    def __init__(self, weight=None, 
-                 gamma=2., reduction='none'):
-        nn.Module.__init__(self)
-        self.weight = weight
+    def __init__(self, gamma=0, alpha=None, size_average=True):
+        super(FocalLoss, self).__init__()
         self.gamma = gamma
-        self.reduction = reduction
-        
-    def forward(self, input_tensor, target_tensor):
-        log_prob = F.log_softmax(input_tensor, dim=-1)
-        prob = torch.exp(log_prob)
-        return F.nll_loss(
-            ((1 - prob) ** self.gamma) * log_prob, 
-            target_tensor, 
-            weight=self.weight,
-            reduction = self.reduction
-        )
+        self.alpha = alpha
+        if isinstance(alpha,(float,int,long)): self.alpha = torch.Tensor([alpha,1-alpha])
+        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+        self.size_average = size_average
+
+    def forward(self, input, target):
+        if input.dim()>2:
+            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
+        target = target.view(-1,1)
+
+        logpt = F.log_softmax(input)
+        logpt = logpt.gather(1,target)
+        logpt = logpt.view(-1)
+        pt = Variable(logpt.data.exp())
+
+        if self.alpha is not None:
+            if self.alpha.type()!=input.data.type():
+                self.alpha = self.alpha.type_as(input.data)
+            at = self.alpha.gather(0,target.data.view(-1))
+            logpt = logpt * Variable(at)
+
+        loss = -1 * (1-pt)**self.gamma * logpt
+        if self.size_average: return loss.mean()
+        else: return loss.sum()
+
 
 class IOULoss(nn.Module):
     def __init__(self, loc_loss_type):
@@ -298,7 +313,7 @@ class FCOSLoss(nn.Module):
         box_targets_flat = torch.cat(box_targets_flat, 0)
 
         pos_id = torch.nonzero(labels_flat > 0).squeeze(1)
-        cls_loss = nn.CrossEntropyLoss()(cls_flat.view(-1, n_class), labels_flat.long().view(-1))
+        cls_loss = FocalLoss(alpha = 0.25, gamma = 2)(cls_flat.view(-1, n_class), labels_flat.long().view(-1))
 
         box_flat = box_flat[pos_id]
         center_flat = center_flat[pos_id]
