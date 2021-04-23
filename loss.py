240b1ca3af1d96939f04c30dd6ebbc4fd1622462
karan-sarkar <karan@sarkarfamily.org>
2021-04-23 10:51:01 -0400

    Fix Focal Loss.

--- loss.py
+++ loss.py
@@ -11,9 +11,7 @@ class FocalLoss(nn.Module):
     def __init__(self, gamma=0, alpha=None, size_average=True):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
-        self.alpha = alpha
-        if isinstance(alpha,(float,int,long)): self.alpha = torch.Tensor([alpha,1-alpha])
-        if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
+        self.alpha = torch.Tensor([alpha,1-alpha])
         self.size_average = size_average
 
     def forward(self, input, target):
