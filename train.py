65194a441ad9eddab1767a8b216e40b89d886373
karan-sarkar <karan@sarkarfamily.org>
2021-04-23 10:47:42 -0400

    Move testing.

--- train.py
+++ train.py
@@ -284,9 +284,9 @@ if __name__ == '__main__':
     else:
         args.ckpt = 0
     for epoch in range(args.epoch):
-        valid(args, epoch, source_valid_loader, source_valid_set, model, device)
-        valid(args, epoch, target_valid_loader, target_valid_set, model, device)
         train(args, epoch, source_loader, target_loader, model, optimizer, device)
         torch.save(model, 'mcd_bdd100k_' + str(args.ckpt + epoch + 1) + '.pth')
+        valid(args, epoch, source_valid_loader, source_valid_set, model, device)
+        valid(args, epoch, target_valid_loader, target_valid_set, model, device)
 
         scheduler.step()
