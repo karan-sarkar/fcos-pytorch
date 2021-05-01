5b681d8f0f133525ad4319ee83b767bbfafe409e
karan-sarkar <karan@sarkarfamily.org>
2021-05-01 08:18:15 -0400

    Move testing

--- train.py
+++ train.py
@@ -320,10 +320,10 @@ if __name__ == '__main__':
     model = model.to(device)
     
     for epoch in range(args.epoch):
-        train(args, epoch, source_loader, target_loader, model, optimizer, optimizer2, device)
-        torch.save((model, optimizer, optimizer2), 'fcos_' + str(args.ckpt + epoch + 1) + '.pth')
         valid(args, epoch, source_valid_loader, source_valid_set, model, device)
         valid(args, epoch, target_valid_loader, target_valid_set, model, device)
+        train(args, epoch, source_loader, target_loader, model, optimizer, optimizer2, device)
+        torch.save((model, optimizer, optimizer2), 'fcos_' + str(args.ckpt + epoch + 1) + '.pth')
         scheduler.step()
 
 
