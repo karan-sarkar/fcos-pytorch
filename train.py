4e7324d84604cbadf6468bae5e69d4aa4e1adad7
karan-sarkar <karan@sarkarfamily.org>
2021-05-04 10:18:29 -0400

    Move validation

--- train.py
+++ train.py
@@ -321,10 +321,10 @@ if __name__ == '__main__':
     model = model.to(device)
     
     for epoch in range(args.epoch):
-        valid(args, epoch, source_valid_loader, source_valid_set, model, device)
-        valid(args, epoch, target_valid_loader, target_valid_set, model, device)
         train(args, epoch, source_loader, target_loader, model, optimizer, optimizer2, optimizer3, device)
         torch.save((model, optimizer, optimizer2, optimizer3), 'fcos_' + str(args.ckpt + epoch + 1) + '.pth')
+        valid(args, epoch, source_valid_loader, source_valid_set, model, device)
+        valid(args, epoch, target_valid_loader, target_valid_set, model, device)
         scheduler.step()
 
 
