e440482a982f504249e83ef04c79ddc5edfc27ca
karan-sarkar <karan@sarkarfamily.org>
2021-05-19 10:35:45 -0400

    Detach mask

--- train.py
+++ train.py
@@ -114,7 +114,7 @@ def harden(cls_pred, device):
     
     
     mx = torch.argmax(cls_p, 1)
-    mask = cls_p.max(1)[0].ge(0.05).float()
+    mask = cls_p.max(1)[0].ge(0.05).float().detach()
     mx = F.one_hot(mx, 10)
     return (torch.mean(torch.abs(cls_p -  mx), 1) * mask).sum() / (mask.sum() + 1)
     
