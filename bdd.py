97a0fcef590346f279e6057dd734c801720837ea
karan-sarkar <karan@sarkarfamily.org>
2021-05-24 11:36:33 -0400

    Add online kmeans clustering

---
+++ bdd.py
@@ -0,0 +1,114 @@
+import json
+import os
+from pathlib import Path
+
+import numpy as np
+import torchvision
+from PIL import Image
+import torchvision
+import tqdm
+
+import torch
+from torch import Tensor, nn
+from torch.utils.data import Dataset
+from util import *
+
+
+def get_ground_truths(train_img_path_list, idx, anno_data):
+
+    classes = {}
+    
+    total_bboxes, total_labels, attributes = [], [], []
+    
+    for i in tqdm.tqdm(idx):
+        bboxes = []
+        labels = []
+        
+        attr = []
+        for keyword in ['weather', 'scene', 'timeofday']:
+            if 'attributes' in anno_data[i] and keyword in anno_data[i]['attributes']:
+                attr.append(anno_data[i]['attributes'][keyword])
+            else:
+                attr.append('')
+        
+        for j in range(len(anno_data[i]["labels"])):
+            if "box2d" in anno_data[i]["labels"][j]:
+                xmin = anno_data[i]["labels"][j]["box2d"]["x1"]
+                ymin = anno_data[i]["labels"][j]["box2d"]["y1"]
+                xmax = anno_data[i]["labels"][j]["box2d"]["x2"]
+                ymax = anno_data[i]["labels"][j]["box2d"]["y2"]
+                bbox = [xmin, ymin, xmax, ymax]
+                category = anno_data[i]["labels"][j]["category"]
+                if category not in classes:
+                    classes[category] = len(classes)
+                cls = classes[category]
+
+                bboxes.append(bbox)
+                labels.append(cls)
+
+        total_bboxes.append(Tensor(bboxes))
+        total_labels.append(Tensor(labels))
+        attributes.append(attr)
+
+    return total_bboxes, total_labels, attributes
+
+
+def _load_json(path_list_idx):
+    with open(path_list_idx, "r") as file:
+        data = json.load(file)
+    print(len(data))
+    return data
+
+
+class BDD(torch.utils.data.Dataset):
+    def __init__(
+        self, img_path, anno_json_path, idx=None):  
+        super(BDD, self).__init__()
+        self.img_path = img_path
+        self.anno_data = _load_json(anno_json_path)
+        self.idx = idx if idx is not None else range(len(self.anno_data))
+        self.total_bboxes_list, self.total_labels_list, self.attributes = get_ground_truths(
+            self.img_path, self.idx, self.anno_data
+        )
+        print(len(self.img_path), len(self.total_labels_list), len(self.total_labels_list))
+
+    def __len__(self):
+        return len(self.img_path)
+
+    def __getitem__(self, idx):
+        img_path = self.img_path[idx]
+        img = Image.open(img_path).convert("RGB")
+
+        labels = self.total_labels_list[idx]
+        bboxes = self.total_bboxes_list[idx]
+        attr = self.attributes[idx]
+        area = (bboxes[:, 3] - bboxes[:, 1]) * (bboxes[:, 2] - bboxes[:, 0])
+
+        boxes = torch.Tensor(bboxes)
+        labels = torch.Tensor(labels).long()
+        
+        return img, boxes, labels, attr
+    
+    def collate_fn(self, batch):
+        """
+        Since each image may have a different number of objects, we need a collate function (to be passed to the DataLoader).
+        This describes how to combine these tensors of different sizes. We use lists.
+        Note: this need not be defined in this Class, can be standalone.
+        :param batch: an iterable of N sets from __getitem__()
+        :return: a tensor of images, lists of varying-size tensors of bounding boxes, labels, and difficulties
+        """
+
+        images = list()
+        boxes = list()
+        labels = list()
+        attr = list()
+
+        for b in batch:
+            images.append(b[0])
+            boxes.append(b[1])
+            labels.append(b[2])
+            attr.append(b[3])
+
+        images = torch.stack(images, dim=0)
+
+        return images, boxes, labels, attr
---
+++ cluster.py
@@ -0,0 +1,131 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.utils as vutils
+import torchvision.models as models
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import json
+import tqdm
+import time
+from collections import Counter
+
+
+
+from bdd import *
+
+CLUSTERS = 10
+BATCH_SIZE = 4
+
+root_img_path = "bdd100k_images/bdd100k/images/100k"
+root_anno_path = "bdd100k/labels"
+
+
+train_anno_json_path = root_anno_path + "/bdd100k_labels_images_train.json"
+val_anno_json_path = root_anno_path + "/bdd100k_labels_images_val.json"
+
+with open(train_anno_json_path, "r") as file:
+    train_data = json.load(file)
+print(len(train_data))
+with open(val_anno_json_path, "r") as file:
+    test_data = json.load(file)
+print(len(test_data))
+
+def make_dataset(train):
+    if train:
+        data = train_data
+        json_file = train_anno_json_path
+        header = train_img_path
+    else:
+        data = test_data
+        json_file = val_anno_json_path
+        header = val_img_path
+    
+    img_list = []
+    for i in tqdm.tqdm(range(len(data))):
+        img_list.append(header + data[i]['videoName'] + '.jpg')
+    dset = BDD(img_list, json_file)
+    return dset
+
+train_dat = make_dataset(True)
+val_dat = make_dataset(False)
+
+def load(dset, sample):
+    return torch.utils.data.DataLoader(dset,batch_size=BATCH_SIZE,shuffle=True, collate_fn=dset.collate_fn)
+
+
+class Backbone(nn.Modul):
+    def __init__(self):
+        super(Backbone, self).__init__()
+
+        backbone = models.resnet50(pretrained=True)
+        layers = list(backbone.children())[:-1]
+        self.num_filters = backbone.fc.in_features
+        self.feature_extractor = nn.Sequential(*layers)
+
+    def forward(self, x):
+        self.feature_extractor.eval()
+        return self.feature_extractor(x).flatten(1)
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+backbone = Backbone()
+FEATURES = backbone.num_filters
+backbone = backbone.to(device)
+means = torch.randn(CLUSTERS, FEATURES).to(device)
+
+
+def train(dataset, model, means):
+    i = 0
+    for images, boxes, labels, attr in tqdm.tqdm(load(dataset)):
+        images = images.to(device)
+        features = model(images)
+        
+        x2 = (features * features).sum(1).view(-1, 1)
+        y2 = (means * means).sum(1).view(1, -1)
+        xy = torch.einsum('bf,kf->bk', features, means)
+        dist = -2 * xy + x2 + y2
+        
+        clusters = F.one_hot(dist.argmin(1), CLUSTERS)
+        change = torch.einsum('bf,bk->kf', features, clusters)
+        means = means * (i/(i + 1)) + change * (1/(i + 1))
+        
+        i += 1
+
+def valid(dataset, model, means):
+    i = 0
+    results = []
+    for images, boxes, labels, attr in tqdm.tqdm(load(dataset)):
+        images = images.to(device)
+        features = model(images)
+        
+        x2 = (features * features).sum(1).view(-1, 1)
+        y2 = (means * means).sum(1).view(1, -1)
+        xy = torch.einsum('bf,kf->bk', features, means)
+        dist = -2 * xy + x2 + y2
+        
+        clusters = dist.argmin(1)
+        for flags in attr:
+            for i in range(len(flags)):
+                if i > len(results) - 1:
+                    results.append(Counter())
+                results[i][flags[i]] += 1
+                results[i]['total'] += 1
+        
+        for res in results:
+            total = res['total']
+            for key in res.keys():
+                if key != 'total':
+                    res[key] /= total
+        
+        print(results)
+        
+        i += 1
+
+for _ in range(100):
+    train(train_dat, backbone, means)
+    valid(val_dat, backbone, means)
