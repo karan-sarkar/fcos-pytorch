f389896885df96da54d33eda4fe32052cac62b67
karan-sarkar <karan@sarkarfamily.org>
2021-05-28 01:55:17 -0400

    Add 5 epochs per amount of clusters

--- cluster.py
+++ cluster.py
@@ -65,57 +65,60 @@ FEATURES = model.num_filters
 model = model.to(device)
 
 
-for CLUSTERS in range(1, 11):
+for CLUSTERS in range(1, 101):
 
     means = None
     counts = None
-    iter = 0
-    loss = []
     
-    print(CLUSTERS)
     
-    pbar = tqdm.tqdm(load(train_dat))
+    print('CLUSTER ', CLUSTERS)
     
-    labels_true = []
-    labels_pred = []
+    for _ in range(5):
     
-    with torch.no_grad():
-        for images, labels in pbar:
-            images = images.to(device)
-            features = style(model(images))
-            if means is None:
-                means = features[:CLUSTERS].to(device)
-                counts = torch.zeros(CLUSTERS).to(device)
-            
-            x2 = (features * features).sum(1).view(-1, 1)
-            y2 = (means * means).sum(1).view(1, -1)
-            xy = torch.einsum('bf,kf->bk', features, means)
-            dist = -2 * xy + x2 + y2
-            
-            loss.append(float(dist.min(1)[0].mean()))
-            avg = sum(loss) / len(loss)
-            res = str(avg)
-            
-            assign = dist.argmin(1)
-            clusters = F.one_hot(assign, CLUSTERS).float()
-            current = clusters.sum(0)
-            counts += current
-            
-            mean_split = [means[i] for i in range(means.size(0))]
-            for i in range(features.size(0)):
-                mean_split[int(assign[i])] += (features[i] - mean_split[int(assign[i])]) / counts[int(assign[i])]
-            means = torch.stack(mean_split, 0)
-            
-            labels_true.append(labels.cpu().detach().numpy())
-            labels_pred.append(assign.cpu().detach().numpy())
-            
-            res += ' ' + str(adjusted_mutual_info_score(np.concatenate(labels_true, 0), np.concatenate(labels_pred, 0)))
-            pbar.set_description(res)
-            
-            iter += 1
+        iter = 0
+        loss = []
+        pbar = tqdm.tqdm(load(train_dat))
+        
+        labels_true = []
+        labels_pred = []
+        
+        with torch.no_grad():
+            for images, labels in pbar:
+                images = images.to(device)
+                features = style(model(images))
+                if means is None:
+                    means = features[:CLUSTERS].to(device)
+                    counts = torch.zeros(CLUSTERS).to(device)
+                
+                x2 = (features * features).sum(1).view(-1, 1)
+                y2 = (means * means).sum(1).view(1, -1)
+                xy = torch.einsum('bf,kf->bk', features, means)
+                dist = -2 * xy + x2 + y2
+                
+                loss.append(float(dist.min(1)[0].mean()))
+                avg = sum(loss) / len(loss)
+                res = str(avg)
+                
+                assign = dist.argmin(1)
+                clusters = F.one_hot(assign, CLUSTERS).float()
+                current = clusters.sum(0)
+                counts += current
+                
+                mean_split = [means[i] for i in range(means.size(0))]
+                for i in range(features.size(0)):
+                    mean_split[int(assign[i])] += (features[i] - mean_split[int(assign[i])]) / counts[int(assign[i])]
+                means = torch.stack(mean_split, 0)
+                
+                labels_true.append(labels.cpu().detach().numpy())
+                labels_pred.append(assign.cpu().detach().numpy())
+                
+                res += ' ' + str(adjusted_mutual_info_score(np.concatenate(labels_true, 0), np.concatenate(labels_pred, 0)))
+                pbar.set_description(res)
+                
+                iter += 1
 
-        print(adjusted_mutual_info_score(np.concatenate(labels_true, 0), np.concatenate(labels_pred, 0)))
-        print(homogeneity_score(np.concatenate(labels_true, 0), np.concatenate(labels_pred, 0)))
+            print(adjusted_mutual_info_score(np.concatenate(labels_true, 0), np.concatenate(labels_pred, 0)))
+            print(homogeneity_score(np.concatenate(labels_true, 0), np.concatenate(labels_pred, 0)))
         
         
     
