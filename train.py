62fd4d382337a51ed767e07690e1b11a0cd0c17a
karan-sarkar <karan@sarkarfamily.org>
2021-06-03 20:41:44 -0400

    Add class loss

--- train.py
+++ train.py
@@ -236,25 +236,27 @@ def train(args, epoch, loader, target_loader, model, c_opt, g_opt, device):
         # Train Bottom
         for j in range(4):
             g_opt.zero_grad()
-            #(loss_dict, _), (loss_dict2, _) = model(images.tensors, targets=targets, r=r)
-            #loss_cls = loss_dict['loss_cls'].mean()
-            #loss_box = loss_dict['loss_box'].mean()
-            #loss_center = loss_dict['loss_center'].mean()
+            (loss_dict, _), (loss_dict2, _) = model(images.tensors, targets=targets, r=r)
+            loss_cls = loss_dict['loss_cls'].mean()
+            loss_box = loss_dict['loss_box'].mean()
+            loss_center = loss_dict['loss_center'].mean()
             
             (_, p), (_, q)  = model(target_images.tensors, targets=target_targets, r=r)
             dloss, mask = compare(p, q)
             dloss += compare(q, p)[0]
-            #loss = loss_cls + loss_box + loss_center
+            loss = loss_cls + loss_box + loss_center
             
-            #loss_cls2 = loss_dict2['loss_cls'].mean()
-            #loss_box2 = loss_dict2['loss_box'].mean()
-            #loss_center2 = loss_dict2['loss_center'].mean()
-            #loss += loss_cls + loss_box + loss_center
+            loss_cls2 = loss_dict2['loss_cls'].mean()
+            loss_box2 = loss_dict2['loss_box'].mean()
+            loss_center2 = loss_dict2['loss_center'].mean()
+            loss += loss_cls2 + loss_box2 + loss_center2
             
-            loss = dloss
+            loss += dloss
             loss.backward()
             nn.utils.clip_grad_norm_(model.parameters(), 10)
             g_opt.step()
+            
+             del loss_cls, loss_box, loss_center, loss_dict2, loss_dict, loss_cls2, loss_box2, loss_center2
         
         
         discrep_loss = dloss.item()
