5370e5aa0e207c7e266b85f9ee33e086ac730431
karan-sarkar <karan@sarkarfamily.org>
2021-06-08 11:56:16 -0400

    Add dashes to argument

--- argument.py
+++ argument.py
@@ -14,8 +14,8 @@ def get_argparser():
     parser.add_argument('--epoch', type=int, default=24)
     parser.add_argument('--n_save_sample', type=int, default=5)
     parser.add_argument('--ckpt', type=int)
-    parser.add_argument('path', type=str)
-    parser.add_argument('path2', type=str)
+    parser.add_argument('--path', type=str)
+    parser.add_argument('--path2', type=str)
 
     return parser
 
