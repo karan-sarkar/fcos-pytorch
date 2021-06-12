8ef02601cd5a61c2da348d76c1ef11c4b9dedae0
karan-sarkar <karan@sarkarfamily.org>
2021-06-12 11:30:55 -0400

    Reduce iterations

--- train.py
+++ train.py
@@ -221,7 +221,7 @@ def train(args, epoch, loader, target_loader, model, c_opt, g_opt, device):
         nn.utils.clip_grad_norm_(model.parameters(), 10)
         c_opt.step()
         
-        for _ in range(5):
+        for _ in range(4):
             g_opt.zero_grad()
             r = torch.range(0, len(targets) - 1).to(device)
             '''
