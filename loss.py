3e7de43863e6e6aad5c1d10353c68f1dbc354377
karan-sarkar <karan@sarkarfamily.org>
2021-06-21 09:47:50 -0400

    More debugging

--- loss.py
+++ loss.py
@@ -280,17 +280,19 @@ class FCOSLoss(nn.Module):
 
         cls_loss = self.cls_loss(cls_flat, labels_flat.int()) / (pos_id.numel() + batch)
 
-        #box_flat = box_flat[pos_id]
-        #center_flat = center_flat[pos_id]
+        box_flat = box_flat[pos_id]
+        center_flat = center_flat[pos_id]
 
-        #box_targets_flat = box_targets_flat[pos_id]
+        box_targets_flat = box_targets_flat[pos_id]
 
         if pos_id.numel() > 0:
             center_targets = self.compute_centerness_targets(box_targets_flat)
 
             box_loss = self.box_loss(box_flat, box_targets_flat, center_targets)
+            print(center_flat)
+            print(center_targets)
             
-            center_loss = self.center_loss(torch.nan_to_num(center_flat), torch.nan_to_num(center_targets))
+            center_loss = self.center_loss(center_flat, center_targets)
 
         else:
             box_loss = box_flat.sum()
