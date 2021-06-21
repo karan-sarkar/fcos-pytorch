21cb393fdb917be987cc0faf1d9d92309d76c80c
karan-sarkar <karan@sarkarfamily.org>
2021-06-21 09:14:35 -0400

    Don't just do on pos id

--- loss.py
+++ loss.py
@@ -280,10 +280,10 @@ class FCOSLoss(nn.Module):
 
         cls_loss = self.cls_loss(cls_flat, labels_flat.int()) / (pos_id.numel() + batch)
 
-        box_flat = box_flat[pos_id]
-        center_flat = center_flat[pos_id]
+        #box_flat = box_flat[pos_id]
+        #center_flat = center_flat[pos_id]
 
-        box_targets_flat = box_targets_flat[pos_id]
+        #box_targets_flat = box_targets_flat[pos_id]
 
         if pos_id.numel() > 0:
             center_targets = self.compute_centerness_targets(box_targets_flat)
