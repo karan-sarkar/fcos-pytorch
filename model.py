eeb9a83d5834d1d111128d242e43c344361e7c90
karan-sarkar <karan@sarkarfamily.org>
2021-06-21 13:29:47 -0400

    Clamp boudnig boxes

--- model.py
+++ model.py
@@ -148,7 +148,7 @@ class FCOSHead(nn.Module):
             centers.append(self.center_pred(cls_out))
 
             bbox_out = self.bbox_tower(feat)
-            bbox_out = torch.exp(scale(self.bbox_pred(bbox_out)))
+            bbox_out = scale(self.bbox_pred(bbox_out)).sigmoid() * 1280
 
             bboxes.append(bbox_out)
 
