2e61b833264c91c4cf896e72e835a53f73c0a401
karan-sarkar <karan@sarkarfamily.org>
2021-06-21 09:39:24 -0400

    Add nan to num

--- loss.py
+++ loss.py
@@ -289,8 +289,8 @@ class FCOSLoss(nn.Module):
             center_targets = self.compute_centerness_targets(box_targets_flat)
 
             box_loss = self.box_loss(box_flat, box_targets_flat, center_targets)
-            print(center_flat, '\n', center_targets)
-            center_loss = self.center_loss(center_flat, center_targets)
+            
+            center_loss = self.center_loss(center_flat.nan_to_num(), center_targets.nan_to_num())
 
         else:
             box_loss = box_flat.sum()
