df8ec5581113fb25c526246a733ab30df439f0dd
karan-sarkar <karan@sarkarfamily.org>
2021-06-27 14:32:01 -0400

    Move validation

--- train.py
+++ train.py
@@ -396,10 +396,10 @@ if __name__ == '__main__':
         g['lr'] = args.lr2
     
     for epoch in range(args.epoch):
-        train(args, epoch, source_loader, target_loader, model, c_opt, g_opt, device)
-        torch.save((model, c_opt, g_opt), 'slim_fcos_' + str(args.ckpt + epoch + 1) + '.pth')
         valid(args, epoch, source_valid_loader, source_valid_set, model, device)
         valid(args, epoch, target_valid_loader, target_valid_set, model, device)
+        train(args, epoch, source_loader, target_loader, model, c_opt, g_opt, device)
+        torch.save((model, c_opt, g_opt), 'slim_fcos_' + str(args.ckpt + epoch + 1) + '.pth')
         
         
        
