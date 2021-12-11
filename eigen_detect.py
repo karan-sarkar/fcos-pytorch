4dbfe8dc170178aea3d9c9f8b57116d1e3e08be1
karan-sarkar <42609534+karan-sarkar@users.noreply.github.com>
2021-12-11 00:43:58 -0500

    Use Resnet Backbone

--- eigen_detect.py
+++ eigen_detect.py
@@ -11,76 +11,7 @@ from loss import FCOSLoss
 from postprocess import FCOSPostprocessor
 from boxlist import BoxList
 
-def init_conv_kaiming(module):
-    if isinstance(module, nn.Conv2d):
-        nn.init.kaiming_uniform_(module.weight, a=1)
 
-        if module.bias is not None:
-            nn.init.constant_(module.bias, 0)
-
-class FPN(nn.Module):
-    def __init__(self, in_channels, out_channel, top_blocks=None):
-        super().__init__()
-
-        self.inner_convs = nn.ModuleList()
-        self.out_convs = nn.ModuleList()
-        #print(in_channels)
-        for i, in_channel in enumerate(in_channels[1:], 1):
-            if in_channel == 0:
-                self.inner_convs.append(None)
-                self.out_convs.append(None)
-
-                continue
-
-            inner_conv = nn.Conv2d(in_channel, out_channel, 1)
-            feat_conv = nn.Conv2d(out_channel, out_channel, 3, padding=1)
-
-            self.inner_convs.append(inner_conv)
-            self.out_convs.append(feat_conv)
-
-        self.apply(init_conv_kaiming)
-
-        self.top_blocks = top_blocks
-
-    def forward(self, inputs):
-        inner = self.inner_convs[-1](inputs[-1])
-        outs = [self.out_convs[-1](inner)]
-
-        for feat, inner_conv, out_conv in zip(
-            inputs[:-1][::-1], self.inner_convs[:-1][::-1], self.out_convs[:-1][::-1]
-        ):
-            if inner_conv is None:
-                continue
-
-            upsample = F.interpolate(inner, scale_factor=2, mode='nearest')
-            inner_feat = inner_conv(feat)
-            inner = inner_feat + upsample
-            outs.insert(0, out_conv(inner))
-
-        if self.top_blocks is not None:
-            top_outs = self.top_blocks(outs[-1], inputs[-1])
-            outs.extend(top_outs)
-
-        return outs
-
-class FPNTopP6P7(nn.Module):
-    def __init__(self, in_channel, out_channel, use_p5=True):
-        super().__init__()
-
-        self.p6 = nn.Conv2d(in_channel, out_channel, 3, stride=2, padding=1)
-        self.p7 = nn.Conv2d(out_channel, out_channel, 3, stride=2, padding=1)
-
-        self.apply(init_conv_kaiming)
-
-        self.use_p5 = use_p5
-
-    def forward(self, f5, p5):
-        input = p5 if self.use_p5 else f5
-
-        p6 = self.p6(input)
-        p7 = self.p7(F.relu(p6))
-
-        return p6, p7
     
 def binary(x, bits):
     mask = 2**torch.arange(bits).to(x.device, x.dtype)
@@ -109,12 +40,10 @@ def pad(x, sh):
 class EigenDetect(nn.Module):
     def __init__(self, config, backbone):
         super().__init__()
-
-        self.backbone = backbone
-        fpn_top = FPNTopP6P7(
-            config.feat_channels[-1], config.out_channel, use_p5=config.use_p5
-        )
-        self.fpn = FPN(config.feat_channels, config.out_channel, fpn_top)
+        
+        self.backbone = torch.nn.Sequential(*(list(backbone.children())[:-1]))
+        
+        
         self.fc1 = nn.Linear(config.out_channel * 5033, 4000) 
         self.fc2 = nn.Linear(4000, 2000)
         self.size = config.n_class -1 + 1 * 4
@@ -135,13 +64,8 @@ class EigenDetect(nn.Module):
     
     def forward(self, input, image_sizes=None, targets=None):
         features = self.backbone(input)
-        #print([f.shape for f in features])
-        #features = torch.stack(features, 0)
-        features = self.fpn(features)
-        #print([f.shape for f in features])
-        features = torch.cat([f.view(f.size(0), f.size(1), -1) for f in features], -1)
-        #print(features.shape)
-        matrix = (self.fc1(features.view(features.size(0), -1).relu()))
+        print(features.shape)
+        matrix = (self.fc1(features.relu()))
         matrix = self.fc2(matrix.relu())
         A, B = self.fc3(matrix.relu()), self.fc4(matrix.relu())
         A = A.view(A.size(0), self.size, self.size)
